dc3728a17c3a91e8f94450e680e286e35b0d86f1
pomkos <albeithome@gmail.com>
2021-01-16 02:15:31 +0000

    initial commit

---
+++ box.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import sqlalchemy as sq
+import datetime as dt
+    
+st.set_page_config(page_title='Suggestion Box')
+
+class dbStuff():
+    def __init__(self):
+        '''
+        Initialize the sqlite database
+        '''
+        # initialize engine
+        engine = sq.create_engine(f'sqlite:///data/box.db')
+        cnx = engine.connect()
+        meta = sq.MetaData()
+        meta.reflect(bind=engine)
+        
+        self.cnx = cnx
+        self.table = meta.tables['suggestions']
+
+    def save_table(self, new_info):
+        '''
+        Saves information to database
+        '''
+        import datetime as dt
+        conn = self.cnx
+        conn.execute(self.table.insert(), [new_info])
+
+    def show_table(self):
+        
+        import pandas as pd
+        table = self.table
+        request = sq.select([table])
+        resultset = self.cnx.execute(request).fetchall()
+        df = pd.DataFrame(resultset)
+        df.columns = resultset[0].keys()
+        return df
+    
+def sanity_checker(url: str):
+    '''
+    Checks for proper formatting of user inputs
+    '''
+    if 'https://' not in url:
+        st.error("Wait a second, this isn't a proper uniform resource locator! [1](https://en.wikipedia.org/wiki/URL)")
+        st.stop()
+        
+d = dbStuff()
+
+st.title("Suggestive  Box!")
+st.write("What's something cool you would like to see in-depth analysis of?")
+
+colm, colt = st.beta_columns(2)
+
+with colm:
+    describe = st.text_input("Brief description*")
+    
+with colt:
+    options = ['Leisure','Friends','Politics','Health/Fitness','Travel (US/World)','Covid','Finance']
+    options.sort()
+    options += ['Other']
+    tag = st.selectbox("Category*", options=options)
+
+url = st.text_input("Website URL with more data (Optional)")
+
+if not url:
+    url = ''
+    
+table_area = st.empty()
+table_area.table(d.show_table())
+
+submit = st.button("Submit")
+new_info = {
+    'description':describe,
+    'category':tag,
+    'url':url,
+    'date':dt.datetime.now().date()
+}
+
+if submit:
+    sanity_checker(url)
+    try:
+        d.save_table(new_info)
+        table_area.table(d.show_table())
+        st.success('Thank you!!')
+        st.balloons()
+    except:
+        st.error('Something bad happened, tell Pete quick!!')
